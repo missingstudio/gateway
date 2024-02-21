ac76884095ce0da0c5195b5af04f97db846c5ff5
Praveen Yadav <pyadav9678@gmail.com>
2024-02-21 10:41:53 +0530

    chore(console): make flat header link
    Signed-off-by: Praveen Yadav <pyadav9678@gmail.com>

--- frontend/apps/console/components/header.tsx
+++ frontend/apps/console/components/header.tsx
@@ -3,11 +3,6 @@ import React, { useMemo } from "react";
 import { Toggle } from "~/components/theme";
 
 import { Button } from "@missingstudio/ui/button";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@missingstudio/ui/popover";
 import { Blocks, BookAIcon, Shapes } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -54,33 +49,21 @@ export default function Header() {
         <Link href="/">AIstudio</Link>
       </div>
       <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center gap-4">
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button asChild key={"/playground"} variant="ghost">
-              <div>
-                {React.createElement(page.icon, { className: "mr-2 h-5 w-5" })}
-                {page.name}
-              </div>
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="flex flex-col gap-2 w-48">
-            {Object.values(navigations).map((link) => (
-              <Button
-                asChild
-                key={link.route}
-                variant="ghost"
-                className="justify-start"
-              >
-                <Link href={link.route}>
-                  {React.createElement(link.icon, {
-                    className: "mr-2 h-5 w-5",
-                  })}
-                  {link.name}
-                </Link>
-              </Button>
-            ))}
-          </PopoverContent>
-        </Popover>
+        {Object.values(navigations).map((link) => (
+          <Button
+            asChild
+            key={link.route}
+            variant="ghost"
+            className="justify-start"
+          >
+            <Link href={link.route}>
+              {React.createElement(link.icon, {
+                className: "mr-2 h-5 w-5",
+              })}
+              {link.name}
+            </Link>
+          </Button>
+        ))}
 
         {links.map((link) => (
           <Button asChild key={link.route} variant="ghost">
